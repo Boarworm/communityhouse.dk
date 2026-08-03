1550c57d38eb44531bf25078318a8fbab58a9024
Boarworm <klepaysayt@gmail.com>
2026-08-03 12:57:10 +0200

    feat: enable fade transition effect for slider component

--- themes/main/partials/blocks/slider/slider.js
+++ themes/main/partials/blocks/slider/slider.js
@@ -1,14 +1,19 @@
 import Swiper from 'swiper';
-import {Autoplay, Navigation} from 'swiper/modules';
+import {Autoplay, Navigation, EffectFade} from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/effect-fade';
 
 export default function init(element) {
     new Swiper(element, {
-        modules: [Autoplay, Navigation],
+        modules: [Autoplay, Navigation, EffectFade],
         loop: true,
         slidesPerView: 1,
         speed: 1500,
         autoplay: true,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
